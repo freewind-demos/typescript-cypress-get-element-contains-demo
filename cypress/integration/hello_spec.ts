b532008b1999652e2cc4ef5ad8000a51559a170f
Freewind <nowindlee@gmail.com>
2019-06-05 17:36:36 +0800

    demo

--- cypress/integration/hello_spec.ts
+++ cypress/integration/hello_spec.ts
@@ -1,39 +1,11 @@
-import {add} from '../../add'
+describe('cypress', () => {
 
-describe('TypeScript', () => {
-  it('works', () => {
-    // note TypeScript definition
-    const x: number = 42
-  })
-
-  it('checks shape of an object', () => {
-    const object = {
-      age: 21,
-      name: 'Joe',
-    }
-    expect(object).to.have.all.keys('name', 'age')
-  })
-
-  it('uses cy commands', () => {
-    cy.wrap({}).should('deep.eq', {})
-  })
-
-  it('tests our example site', () => {
-    cy.visit('https://example.cypress.io/')
-    cy.get('.home-list')
-      .contains('Querying')
-      .click()
-    cy.get('#query-btn').should('contain', 'Button')
-  })
-
-  // enable once we release updated TypeScript definitions
-  it('has Cypress object type definition', () => {
-    expect(Cypress.version).to.be.a('string')
-  })
-
-
-  it('adds numbers', () => {
-    expect(add(2, 3)).to.equal(5)
+  it('should get element and print it', () => {
+    cy.visit('index.html');
+    cy.get('#main')
+      .then(element => {
+        console.log(element)
+      })
   })
 
 })
